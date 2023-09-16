d3cdd324079a032bbe23ae5d6e18934001cfc043
Tam Tam <thanhtam.tes@gmail.com>
2023-09-16 02:56:52 -0600

    Headless browsers

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -2,7 +2,11 @@ package commons;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseTest {
 
@@ -13,17 +17,48 @@ public class BaseTest {
 	protected WebDriver getBrowserDriver(String browserName) {
 		
 		if (browserName.equals("firefox")) {
+				WebDriverManager.firefoxdriver().setup();
+				driver = new FirefoxDriver();
+			
+			//headless firefox
+		} else if (browserName.equals("h_firefox")) {
 			if (osName.contains("Mac OS")) {
 				System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
-				driver = new FirefoxDriver();
+				FirefoxOptions options = new FirefoxOptions();
+				options.addArguments("--headless");
+				options.addArguments("window-size=1920×1080");
+				driver = new FirefoxDriver(options);
 			} else {
 				System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\\\geckodriver.exe");
-				driver = new FirefoxDriver();
+				FirefoxOptions options = new FirefoxOptions();
+				options.addArguments("--headless");
+				options.addArguments("window-size=1920×1080");
+				driver = new FirefoxDriver(options);
 			}
 			
-		} else if (browserName.equals("chrome")) {
+			
+			//headless chrome
+			//recheck version chrome driver before run
+		}
+		else if (browserName.equals("h-chrome")) {
 			if (osName.contains("Mac OS")) {
 				System.setProperty("webdriver.chrome.driver", projectPath + "/browserDrivers/chromedriver");
+				ChromeOptions options = new ChromeOptions();
+				options.addArguments("--headless");
+				options.addArguments("window-size=1920×1080");
+				driver = new ChromeDriver(options);
+			} else {
+				System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\\\chromedriver.exe");
+				ChromeOptions options = new ChromeOptions();
+				options.addArguments("--headless");
+				options.addArguments("window-size=1920×1080");
+				driver = new ChromeDriver(options);
+			}
+			
+		}else if (browserName.equals("chrome")) {
+			if (osName.contains("Mac OS")) {
+				System.setProperty("webdriver.chrome.driver", projectPath + "/browserDrivers/chromedriver");
+				//WebDriverManager.chromedriver().setup();
 				driver = new ChromeDriver();
 			} else {
 				System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\\\chromedriver.exe");
--- testcases/com/nopcommerce/user/User_01__Register_Page_Object.java
+++ testcases/com/nopcommerce/user/User_01__Register_Page_Object.java
@@ -2,35 +2,33 @@ package com.nopcommerce.user;
 
 import org.testng.annotations.Test;
 
-import commons.BasePage;
+import commons.BaseTest;
 import pageObjects.HomePageObject;
 import pageObjects.RegisterPageObject;
 
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 
-import java.security.Timestamp;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 
-public class User_01__Register_Page_Object {
+public class User_01__Register_Page_Object extends BaseTest {
 	
 	private WebDriver driver;
 	private String emailAddress, firstName, lastName, password;
 	private HomePageObject homePage;
 	private RegisterPageObject registerPage;
-	private String projectPath = System.getProperty("user.dir");
 
+	@Parameters("browser")
 	@BeforeClass
-	public void beforeClass() {
+	public void beforeClass(String browserName) {
 
-		System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
-		driver = new FirefoxDriver();
+		driver = getBrowserDriver(browserName);
+		
 		homePage = new HomePageObject(driver);
 		registerPage = new RegisterPageObject(driver);
 		
